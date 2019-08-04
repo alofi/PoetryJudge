9482fa4d884133cf67770a0bc4881a43ecbc6f74
Abdulrahman <mr.alofi5@gmail.com>
2019-08-04 15:54:35 +0300

    initial commit

---
+++ PoetryJudge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoetryJudge.Startup))]
+namespace PoetryJudge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
